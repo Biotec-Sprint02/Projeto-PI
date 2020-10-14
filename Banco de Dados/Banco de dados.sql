4a0fb6001d77c48fb8212f0735432954bdfab421
Luiz-Gustavo-Silva <luiz.gsilva@bandtec.com.br>
2020-10-13 21:54:56 -0300

    Atualizacao final do banco de dados

--- Banco de Dados/Banco de dados.sql
+++ Banco de Dados/Banco de dados.sql
@@ -1,90 +1,100 @@
 create database BioTec;
+
 use BioTec;
 
-create table login(
-idLogin int primary key,
-email varchar (55),
-senha varchar (20)
-);
-
-create table trajeto(
-idViagem int primary key auto_increment,
-inicio varchar(50),
-destino varchar(50),
-horasaida datetime,
-horachegada datetime,
-fkMotorista int,
-fkLote int,
-fkSensor int,
-foreign key(fkMotorista) references motorista(idMotorista),
-foreign key(fkLote) references lote(idLote),
-foreign key(fkSensor) references sensor(idSensor)
-)auto_increment = 100000;
-
-insert into login values
-(100, 'joãobatista@gmail.com', '12345678'),
-(101, 'paulacarvalho@outlook.com', '25262728'),
-(102, 'gabrielamiranda@gmail.com', '35363637'),
-(103, 'caiogomesvieira@gmail.com', '46589302'); 
-
-select * from trajeto;
-desc trajeto;
-
-create table empresa(
-idEmpresa int primary key,
+create table Empresa(
+idEmpresa int primary key auto_increment,
 nomeEmpresa varchar(40),
 cep varchar(40)
-);
+)auto_increment = 1000;
 
-insert into empresa values 
-(1000, 'Via Expressa', '02052-001')
-,(2000, 'Raizes Transportes', '21011-180')
-,(3000, 'TransNova', '04782-000')
-,(4000, 'BCL express', '04561-060');
+select * from Empresa;
 
-select * from empresa;
-
-create table motorista(
-IdMotorista int primary key, -- Colocamos isso pois no CPF ja tem CNH e o RG porem n sabemos se sera isso.
+create table Motorista(
+cpfMotorista char(15) primary key,
 nomeMotorista varchar (40),
-CPFmotorista char (15),  
 modeloVeiculo varchar (40)
 );
 
+select * from Motorista;
 
-insert into motorista values 
-(1, 'Gael Davi Hugo','077.491.058-59', 'Caminhao', '2020-09-09', '2020-10-10')
-,(2, 'Enzo Ruan Pinto','520.203.278-13', 'Carro', '2020-10-10', '2020-11-11') 
-,(3, 'Augusto Fernando Alves','235.868.078-80', 'Moto', '2020-11-11', '2020-12-12')
-,(4, 'Theo Guilherme José','538.790.158-72', 'Caminhao', '2020-12-12', '2020-12-13');
-
-select * from motorista;
-
-create table lote(
-idLote int primary key,
+create table Lote(
+idLote int primary key auto_increment,
 nomeRemedio varchar (40),
 quantidade int
-);
+)auto_increment = 50000;
 
+select * from Lote;
 
-insert into lote values
-(122, 'Insulina', '50')
-,(344, 'Xalacon', '55')
-,(566, 'Miacalcic Spray nasal', '60')
-,(788,'Foraseq', '65');
-
-select * from lote;
-
-create table sensor(
-idSensor int primary key, -- Sensor DhT11
+create table dadosAPI(
+idDados int primary key auto_increment, -- Sensor DhT11
+sensor varchar(10),
 temperatura float,
 umidade varchar(20)
+)auto_increment = 100000;
+
+select * from dadosAPI;
+
+create table Login(
+fkcpfMotorista char(15),
+email varchar (55),
+senha varchar (40),
+foreign key (fkcpfMotorista) references Motorista(cpfMotorista)
 );
 
-insert into sensor values
-(001,2.2,'3%')
-,(002,4.0,'2%')
-,(003,6.1,'1%')
-,(004,7.6,'0%');
+select * from Login;
 
-select * from sensor;
+create table Trajeto(
+idViagem int primary key auto_increment,
+inicio varchar(50),
+destino varchar(50),
+horaSaida datetime,
+horaChegada datetime,
+fkcpfMotorista char(15),
+fkLote int,
+fkDados int,
+foreign key(fkcpfMotorista) references Motorista(cpfMotorista),
+foreign key(fkLote) references Lote(idLote),
+foreign key(fkDados) references dadosAPI(idDados)
+)auto_increment = 500000;
+
+select * from Trajeto;
+desc Trajeto;
+
+-- --------------------------------------------------------------------------------------------------------------------------
+-- ==========================================================================================================================
+-- 											Separação das Tabelas e dos Inserts
+-- ==========================================================================================================================
+-- --------------------------------------------------------------------------------------------------------------------------
+
+insert into Empresa values 
+(null, 'XMan-Evolution', '09361-110');
+
+select * from Empresa;
+
+insert into Motorista values 
+(23745527802 , 'José das Couves' , 'Ford - R440');
+
+select * from Motorista;
+
+insert into Lote values
+(null, 'Decadron', '50');
+
+select * from Lote;
+
+insert into dadosAPI values
+(null,'DHT11 - 1' , '1.5' , '30%');
+
+select * from dadosAPI;
+
+insert into Login values
+(23745527802, 'Xman@evolution.com', 'Versão Brasileira Hebbert Richards');
+
+select * from Login,Motorista where fkcpfMotorista = cpfMotorista;
+
+insert into Trajeto values
+(null , 'Av. das Magnólias - Jardim Cardoso nº1010' , 'Itaquaquecetuba - Esqueci nº15' , 
+'2020-05-25 08:30:05' , '2020-05-26 00:31:05' , 23745527802 , 50000 , 100000 );
+
+select * from Trajeto,Motorista,Lote,dadosAPI where fkcpfMotorista = cpfMotorista and fkLote = idLote
+and fkDados = idDados;
